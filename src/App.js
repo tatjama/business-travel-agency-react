e88b8fb00b51644f59dc256ee8ee174ab5acbe29
tatjama <tanja120a@gmail.com>
2020-08-16 18:02:13 +0200

    Deleted React icons

--- src/App.js
+++ src/App.js
@@ -4,24 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello World</p>
-        <p>Tanja</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />       
+        <p>Hello World</p>        
       </header>
     </div>
+     
   );
 }
 
