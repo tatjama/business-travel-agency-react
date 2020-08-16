051f056389b334ba3846ae2f17c6ccd97ccb23a3
tatjama <tanja120a@gmail.com>
2020-08-16 18:53:49 +0200

    Start app
    Removed files that is not needed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App(props) {
+ const subject = props.subject;
+    
   return (
-    <div className="App">      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />       
-        <p>Hello World</p>        
+    <div className="App">   
+    
+      <header className="App-header"><p>Hello, {subject}</p>        
       </header>
     </div>
      
