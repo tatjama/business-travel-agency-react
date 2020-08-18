494fca1cb6db18b6f57cb92fcacbf86c89fae8c7
tatjama <tanja120a@gmail.com>
2020-08-18 15:07:06 +0200

    Start App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-function App(props) {
- const subject = props.subject;
-    
-  return (
-    <div className="App">   
-    
-      <header className="App-header"><p>Hello, {subject}</p>        
-      </header>
+ function App(props){
+  return(
+    <div>
+      Hello App! {props.name}
     </div>
-     
-  );
+  )
+   
 }
 
-export default App;
+export default App
