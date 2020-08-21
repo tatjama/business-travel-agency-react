d9f7b35b4e2f900d4c98db1286a012fcbb16d06f
tatjama <tanja120a@gmail.com>
2020-08-21 07:36:15 +0200

    SignUp.js updated
    Make onChange and onSubmit to signUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function SignUp(){
+
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordRepeat: "",
+    address: "",
+    addressAlternative: "",
+    phone: "",
+    phoneAlternative: ""
+  })
+
+  const handleChange = (e)=>{
+    const {name, value} = e.target;
+    console.log(name);
+    console.log(value);
+    setValues({
+      ...values,
+      [name]: value
+    })    
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    signUp();
+  }
 
     //onClick
   
@@ -29,49 +56,141 @@ export default function SignUp(){
   }
 
     return(
-        <form className="sign-form" id="sign-up-form" style={{display: "none"}}>
+        <form className="sign-form" id="sign-up-form" onSubmit = {handleSubmit} noValidate /*style={{display: "none"}}*/ >
                    <fieldset className="sign-up-fieldset">                
                         <div id="formMain">
                            <div id="formLeft" >
-                                First name:
+                                
                                 <span className="error-sign-up" title="Error. Valid name contain only letters"></span>
-                                <input type="text"  id="sign-up-name" onBlur={validName('sign-up-name', 0, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 0)} required/>
-                                <br/> Second name:
+                                <input 
+                                  type="text" 
+                                  name="firstName" 
+                                  placeholder=" First name - (required)" 
+                                  id="sign-up-name" 
+                                  onBlur={validName('sign-up-name', 0, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 0)} 
+                                  value = {values.firstName}
+                                  onChange = {handleChange}
+                                  required
+                                />
+                                <br/>
                                 <span className="error-sign-up" title="Error. Valid surname contain only letters"></span>
-                                <input type="text"  id="sign-up-surname" onBlur={validName('sign-up-surname', 1, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 1)} required/>
-                                <br/> E-mail:
+                                <input 
+                                  type="text" 
+                                  name="surname" 
+                                  placeholder=" Last name - (required)" 
+                                  id="sign-up-surname" 
+                                  onBlur={validName('sign-up-surname', 1, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 1)} 
+                                  value={values.lastName}
+                                  onChange = {handleChange}
+                                  required
+                                />
+                                <br/> 
                                 <span className="error-sign-up" title="Error. E-mail format something@something.com"></span>
-                                <input type="email"  id="sign-up-e-mail" onBlur={validEMail('sign-up-e-mail', 2, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 2)} required/>
-                                <br/> Password:
+                                <input 
+                                  type="email" 
+                                  name="email" 
+                                  placeholder=" E-mail - (required)" 
+                                  id="sign-up-e-mail" 
+                                  onBlur={validEMail('sign-up-e-mail', 2, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 2)}
+                                  value = {values.email} 
+                                  onChange = {handleChange}
+                                  required
+                                />
+                                <br/> 
                                 <span className="error-sign-up" title="Error. Password format only letters and numbers"></span>
-                                <input type="password"  id="sign-up-password" name="bday" onBlur={validPassword('sign-up-password', 3, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 3)} required/>
-                                <br/> Repeat Password:
+                                <input 
+                                  type="password" 
+                                  name="password" 
+                                  placeholder=" Password - (required)" 
+                                  id="sign-up-password" 
+                                  onBlur={validPassword('sign-up-password', 3, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 3)}
+                                  value = {values.password} 
+                                  onChange = {handleChange}
+                                  required
+                                />
+                                
                                 <span className="error-sign-up" title="Error. Password format only letters and numbers"></span>
-                                <input type="password"  id="sign-up-password-r" name="bday" onBlur={validPassword('sign-up-password-r', 4, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 4)} required/>
+                                <input 
+                                  type="password" 
+                                  name="passwordRepeat" 
+                                  placeholder=" Repeat password - (required)" 
+                                  id="sign-up-password-r" 
+                                  onBlur={validPassword('sign-up-password-r', 4, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 4)}
+                                  value = {values.passwordRepeat} 
+                                  onChange = {handleChange}
+                                  required
+                                />
                             </div>
 
                             <div id="formRight" >
-                                Adress 1:
+                                
                                 <span className="error-sign-up" title="Error. Address contain only letters and numbers"></span>
-                                <input type="text" placeholder="(required)" id="address1" onBlur={validPassword('address1', 5, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 5)} required/>
-                                <br/> Adress 2:
+                                <input 
+                                  type="text" 
+                                  name="address" 
+                                  placeholder=" Address 1 - (required)" 
+                                  id="address1" 
+                                  onBlur={validPassword('address1', 5, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 5)}
+                                  value = {values.address} 
+                                  onChange = {handleChange}
+                                  required
+                                />
+                                <br/> 
                                 <span className="error-sign-up" title="Error. Address contain only letters and numbers"></span>
-                                <input type="text" id="address2" onBlur={validPassword('address2', 6, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 6)}/>
-                                <br/> Phone number 1:
+                                <input 
+                                  type="text" 
+                                  name="addressAlternative" 
+                                  placeholder=" Address 2" 
+                                  id="address2" 
+                                  onBlur={validPassword('address2', 6, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 6)}
+                                  value = {values.addressAlternative}
+                                  onChange = {handleChange}
+                                />
+                                <br/> 
                                 <span className="error-sign-up" title="Error. Phone contain only numbers"></span>
-                                <input type="number" minLength="11" maxLength="15" placeholder="(required)" id="phone1" onBlur={validPhone('phone1', 7, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 7)} required/>
-                                <br/> Phone number 2:
+                                <input 
+                                  type="number" 
+                                  name="phone" 
+                                  minLength="11" 
+                                  maxLength="15" 
+                                  placeholder=" Phone 1 - (required)" 
+                                  id="phone1" 
+                                  onBlur={validPhone('phone1', 7, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 7)} 
+                                  value = {values.phone}
+                                  onChange = {handleChange}
+                                  required
+                                />
+                                <br/>
                                 <span className="error-sign-up" title="Error. Phone contain only numbers"></span>
-                                <input type="number" minLength="11" maxLength="15"  id="phone2" onBlur={validPhone('phone2', 8, 'error-sign-up')} onFocus={clearonFocus('error-sign-up', 8)}/>
+                                <input 
+                                  type="number" 
+                                  name="phoneAlternative" 
+                                  minLength="11" 
+                                  maxLength="15" 
+                                  placeholder=" Phone 2" 
+                                  id="phone2" 
+                                  onBlur={validPhone('phone2', 8, 'error-sign-up')} 
+                                  onFocus={clearonFocus('error-sign-up', 8)}
+                                  value = {values.phoneAlternative}
+                                  onChange = {handleChange}
+                                />
                                 <br/>
                                 <div style={{display:"none"}}> Status:
-                                <input type="number" placeholder="administrator" id="status" readOnly value="1"/>
+                                <input type="number" name="status" placeholder="administrator" id="status" readOnly value="1"/>
                                 <br/>
                             </div>
                          </div>
                         </div>
                         <div className="buttons">
-                            <button type="button" onClick={signUp}>Sign Up</button>
+                            <button type="submit">Sign Up</button>
                             <button type="reset" >Clear</button>
                             <br/><br/>
                             <input type="button" onClick={hideSignUpForm} value="Close"/>
