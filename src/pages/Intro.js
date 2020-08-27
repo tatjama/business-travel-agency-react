d38d378270b3cb08867444b18b84953dc994dc6f
tatjama <tanja120a@gmail.com>
2020-08-27 17:52:01 +0200

    updated router

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,21 +1,16 @@
 import React from 'react';
 //import '../index.css';
 //import Footer from '../components/Footer';
-import IntroHeader from '../components/IntroHeader';
+//import IntroHeader from '../components/IntroHeader';
 import SignIn from '../components/SignIn';
-import SignUp from '../components/SignUp';
+//import SignUp from '../components/SignUp';
 
 
 export default function Intro(){
     return(
-      <div id = "intro-page-gradient">
-      <IntroHeader/>  
       <div >
         <SignIn/>
-        <div >
-          <SignUp/>
-        </div>            
-      </div>
-    </div>        
+                    
+      </div>        
     )
 }
