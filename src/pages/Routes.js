76431629a82a6aa756009ab6041423a4e68ec358
tatjama <tanja120a@gmail.com>
2020-08-29 09:30:10 +0200

    routing warning fixed

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Redirect} from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Home from './Home';
@@ -10,32 +10,19 @@ import Feedbacks from './Feedbacks';
 import Providers from './Providers';
 //import NotFound from './NotFound';
 
-export default function Routes(props) {
-console.log(props);
+export default function Routes() {
+
   return (
         <Switch>  
             <Route path = "/signIn"  component = {SignIn}/>  
             <Route path = "/signup" component = {SignUp} />
             <Route path = "/" exact  component = {Home}/>         
-                    <Route path = "/life" component = {Life} />
-                    <Route path = "/accommodation" component = {Accommodation}/>
-                    <Route path = "/enjoy" component = {Enjoy}/>
-                    <Route path = "/feedbacks" component = {Feedbacks} />
-                    <Route path = "/providers" component = {Providers} />
-            {/*
-                props.isUserAuthenticated?
-                <>
-                    <Route path = "/" exact  component = {Home}/>         
-                    <Route path = "/life" component = {Life} />
-                    <Route path = "/accommodation" component = {Accommodation}/>
-                    <Route path = "/enjoy" component = {Enjoy}/>
-                    <Route path = "/feedbacks" component = {Feedbacks} />
-                    <Route path = "/providers" component = {Providers} />
-                </>
-                 : <Redirect to="/signin"/>
-            */}                       
-                
-                       <Route  component = {SignIn} />   
+            <Route path = "/life" component = {Life} />
+            <Route path = "/accommodation" component = {Accommodation}/>
+            <Route path = "/enjoy" component = {Enjoy}/>
+            <Route path = "/feedbacks" component = {Feedbacks} />
+            <Route path = "/providers" component = {Providers} />                 
+            <Route  component = {SignIn} />   
         </Switch>
     );
 }
