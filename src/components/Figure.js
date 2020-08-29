186f36a6d1085422f10bf1b9a7a581e5f1384219
tatjama <tanja120a@gmail.com>
2020-08-29 17:56:06 +0200

    Figure component  created

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Figure = (props)=>{
     return(
         <figure>
-        <a href="#transport" onClick = {showLocalTransport}>
-          <img src = {astronaut}  alt="space astronaut"/>
-          <figcaption>Local transport</figcaption>
+        <a href={props.go} onClick = {props.handleOnClick}>
+          <img src = {props.source}  alt={props.alt}/>
+          <figcaption>{props.name}</figcaption>
         </a>
     </figure>
     )
