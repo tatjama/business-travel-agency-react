221bf64dc7fc25780b9d6e97a07db57a9aa1e841
tatjama <tanja120a@gmail.com>
2020-08-31 17:33:06 +0200

    map arrays figureList and comments

--- src/components/SectionFirst.js
+++ src/components/SectionFirst.js
@@ -8,81 +8,105 @@ import airplane from '../images/airplane.webp';
 
 
 class SectionFirst extends React.Component{
+    constructor(){
+        super();
+        
     
+    const showLocalTransport = () => {
+        document.getElementById('transport').style.display = "block";
+        document.getElementById('food').style.display = "none";
+        document.getElementById('safety').style.display = "none";
+        document.getElementById('culture').style.display = "none";
+        document.getElementById('airport').style.display = "none";
+    }
+    const showFood = () => {
+        document.getElementById('transport').style.display = "none";
+        document.getElementById('food').style.display = "block";
+        document.getElementById('safety').style.display = "none";
+        document.getElementById('culture').style.display = "none";
+        document.getElementById('airport').style.display = "none";
+    }
+    const showSafety = () => {
+        document.getElementById('transport').style.display = "none";
+        document.getElementById('food').style.display = "none";
+        document.getElementById('safety').style.display = "block";
+        document.getElementById('culture').style.display = "none";
+        document.getElementById('airport').style.display = "none";
+    }
+    const showLocalCulture = () => {
+        document.getElementById('transport').style.display = "none";
+        document.getElementById('food').style.display = "none";
+        document.getElementById('safety').style.display = "none";
+        document.getElementById('culture').style.display = "block";
+        document.getElementById('airport').style.display = "none";
+    }
+    const showAirport = () => {
+        document.getElementById('transport').style.display = "none";
+        document.getElementById('food').style.display = "none";
+        document.getElementById('safety').style.display = "none";
+        document.getElementById('culture').style.display = "none";
+        document.getElementById('airport').style.display = "block";
+    }
+    this.state = {
+        figureList: [
+            {
+                name: "Local transport", 
+                source: astronaut, 
+                handleOnClick:  showLocalTransport, 
+                alt: "space astronaut", 
+                go:  "#transport"
+
+            },
+            {
+                name: "Food & Entertainment", 
+                source: breakfast,
+                handleOnClick: showFood,
+                alt: "breakfast" ,
+                go: "#food"
+            },
+            {
+                name: "Safety", 
+                source: friends, 
+                handleOnClick: showSafety,
+                alt: "friends hands" ,
+                go: "#safety"
+            },
+            {
+                name: "Local culture" ,
+                source: world, 
+                handleOnClick: showLocalCulture,
+                alt: "world" ,
+                go: "#culture"
+            },
+            {
+                name: "Airport", 
+                source: airplane, 
+                handleOnClick: showAirport,
+                alt: "airplane" ,
+                go: "#airport"
+            }
+        ]
+    }
+
+}
     render(){
-        function showLocalTransport() {
-            document.getElementById('transport').style.display = "block";
-            document.getElementById('food').style.display = "none";
-            document.getElementById('safety').style.display = "none";
-            document.getElementById('culture').style.display = "none";
-            document.getElementById('airport').style.display = "none";
-        }
-        function showFood() {
-            document.getElementById('transport').style.display = "none";
-            document.getElementById('food').style.display = "block";
-            document.getElementById('safety').style.display = "none";
-            document.getElementById('culture').style.display = "none";
-            document.getElementById('airport').style.display = "none";
-        }
-        function showSafety() {
-            document.getElementById('transport').style.display = "none";
-            document.getElementById('food').style.display = "none";
-            document.getElementById('safety').style.display = "block";
-            document.getElementById('culture').style.display = "none";
-            document.getElementById('airport').style.display = "none";
-        }
-        function showLocalCulture() {
-            document.getElementById('transport').style.display = "none";
-            document.getElementById('food').style.display = "none";
-            document.getElementById('safety').style.display = "none";
-            document.getElementById('culture').style.display = "block";
-            document.getElementById('airport').style.display = "none";
-        }
-        function showAirport() {
-            document.getElementById('transport').style.display = "none";
-            document.getElementById('food').style.display = "none";
-            document.getElementById('safety').style.display = "none";
-            document.getElementById('culture').style.display = "none";
-            document.getElementById('airport').style.display = "block";
-        }
-    
+        
         return(
             <section className="section-first">
-                    <Figure 
-                        name = "Local transport" 
-                        source = {astronaut} 
-                        handleOnClick = {showLocalTransport} 
-                        alt = "space astronaut" 
-                        go  = "#transport"
+                {this.state.figureList.map((figure) =>{
+                    return(
+                        <Figure 
+                        name = {figure.name} 
+                        source = {figure.source} 
+                        handleOnClick = {figure.handleOnClick} 
+                        alt = {figure.alt} 
+                        go  = {figure.go}
+                        key = {figure.name}
                     />
-                    <Figure 
-                        name = "Food & Entertainment" 
-                        source = {breakfast} 
-                        handleOnClick = {showFood}
-                        alt = "breakfast" 
-                        go  = "#food"
-                    />
-                    <Figure 
-                        name = "Safety" 
-                        source = {friends} 
-                        handleOnClick = {showSafety}
-                        alt = "friends hands" 
-                        go  = "#safety"
-                    />
-                    <Figure 
-                        name = "Local culture" 
-                        source = {world} 
-                        handleOnClick = {showLocalCulture}
-                        alt = "world" 
-                        go  = "#culture"
-                    />
-                    <Figure 
-                        name = "Airport" 
-                        source = {airplane} 
-                        handleOnClick = {showAirport}
-                        alt = "airplane" 
-                        go  = "#airport"
-                    />
+                    )}
+                )}
+                    
+                    
                 </section>
 
         )
