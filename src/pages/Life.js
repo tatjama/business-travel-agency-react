ccc63b47226c23028c2cc0dc6c3f10d7f1ad65f6
tatjama <tanja120a@gmail.com>
2020-09-01 08:07:04 +0200

    added new commentArrey

--- src/pages/Life.js
+++ src/pages/Life.js
@@ -12,11 +12,17 @@ import SectionFirst from "../components/SectionFirst";
 import avatar from '../images/traveler.webp';
 import avatarMarko from '../images/avatar-marko.webp';
 import avatarJelena from '../images/avatar-jelena.webp';
+import avatarLuka from '../images/avatar-luka.webp';
+import avatarStefan from '../images/avatar-stefan.webp';
+import avatarPetar from '../images/avatar-petar.webp';
+import avatarSanja from '../images/avatar-sanja.jpg';
+import avatarIvana from '../images/avatar-ivana.jpg';
+import avatarDragan from '../images/avatar-dragan.webp';
 
-const  comments = [
+const  comments1 = [
     {
-        id: "comment1" ,
-        source: avatarJelena, 
+        id: "comment-1-1" ,
+        source: avatarPetar, 
         userName: " Pera Peric",
         date: "01.01.2013",
         commentTitle: "I like this city",
@@ -24,8 +30,8 @@ const  comments = [
     
     },
     {
-        id: "comment2" ,
-        source: avatar, 
+        id: "comment-1-2" ,
+        source: avatarJelena, 
         userName: " Dusko Dugousko",
         date: "02.02.2019",
         commentTitle: "Airplane is quite old...",
@@ -33,11 +39,98 @@ const  comments = [
               
     },
     {
-        id: "comment3" ,
+        id: "comment-1-3" ,
         source: avatarMarko,
         userName: " Marko Markovic",
         date: "03.03.2003",
         commentTitle: "About a place",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+               
+    }
+]    
+const  comments2 = [
+    {
+        id: "comment-2-1" ,
+        source: avatarJelena, 
+        userName: " Jelena Jovanovic",
+        date: "01.01.2019",
+        commentTitle: "I like Local food",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+    
+    },
+    {
+        id: "comment-2-2" ,
+        source: avatarLuka, 
+        userName: " Luka Jovovic",
+        date: "02.02.2020",
+        commentTitle: "This restaurant is not clean",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+              
+    },
+    {
+        id: "comment-2-3" ,
+        source: avatarStefan,
+        userName: " Stefan Stefanovic",
+        date: "03.03.2020",
+        commentTitle: "They have the best cakes in the world",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+               
+    }
+]    
+const  comments3 = [
+    {
+        id: "comment-3-1" ,
+        source: avatarSanja, 
+        userName: " Sanja Jovanovic",
+        date: "10.01.2019",
+        commentTitle: "Health care is very organised",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+    
+    },
+    {
+        id: "comment-3-2" ,
+        source: avatarDragan, 
+        userName: " Dragan Jovovic",
+        date: "20.02.2020",
+        commentTitle: "Very calm and safe place",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+              
+    },
+    {
+        id: "comment-3-3" ,
+        source: avatarIvana,
+        userName: " Ivana Stefanovic",
+        date: "03.07.2020",
+        commentTitle: "Police station is in the center of city",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+               
+    }
+]    
+const  comments = [
+    {
+        id: "comment-4-1" ,
+        source: avatarIvana, 
+        userName: " Ivana Jovanovic",
+        date: "10.01.2019",
+        commentTitle: "Fantastic show",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+    
+    },
+    {
+        id: "comment-4-2" ,
+        source: avatar, 
+        userName: " Dragan Jovovic",
+        date: "20.02.2020",
+        commentTitle: "Theatar is very modern",
+        commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
+              
+    },
+    {
+        id: "comment-4-3" ,
+        source: avatarJelena,
+        userName: " Jelena Stefanovic",
+        date: "03.07.2020",
+        commentTitle: "National museum have extensive collection",
         commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat iverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet faucib maximus. Proin non nulla lectus."
                
     }
@@ -70,11 +163,8 @@ const Life = ()=>{
                             li4 = "Taxi"
                             buttonId = "show-add-comment-transport"
                             handleOnClick = {showAddComment}  
-                            arrayComments = {comments} 
-                />   
-
-                      
-                       
+                            arrayComments = {comments1} 
+                />             
                     
                     <GeneralInformation 
                             id = "food" 
@@ -86,7 +176,7 @@ const Life = ()=>{
                             li4 = "Clubs"
                             buttonId = "show-add-comment-food"
                             handleOnClick = {showAddComment}
-                            arrayComments = {comments} 
+                            arrayComments = {comments2} 
                     />
                     
                     <GeneralInformation 
@@ -99,7 +189,7 @@ const Life = ()=>{
                             li4 = "Hospitality"
                             buttonId = "show-add-comment-safety"
                             handleOnClick = {showAddComment}
-                            arrayComments = {comments} 
+                            arrayComments = {comments3} 
                     />
                     
                     <GeneralInformation 
