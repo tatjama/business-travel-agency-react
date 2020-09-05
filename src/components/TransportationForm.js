f9c75c5d4ac5e10abb1fba1a11f070f2a409d5dc
tatjama <tanja120a@gmail.com>
2020-09-05 13:29:11 +0200

    Created components to Transportation Form
    Created TransportationFrom, TransportationTo, TransportationTo components

--- src/components/TransportationForm.js
+++ src/components/TransportationForm.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import TransportationFrom from './TransportationFrom';
+import TransportationTo from './TransportationTo';
+import TransportationType from './TransportationType';
 
 const TransportationForm = () =>{
     function showTransportation(){
@@ -9,87 +12,9 @@ const TransportationForm = () =>{
             <fieldset>
                 <legend>Transportation</legend>
                     <div id="transportationFromTo">
-                        <div id="from">
-                            FROM:
-                            <br/>
-                            Country: 
-                            <input 
-                                list="countryFrom" 
-                                name="countryFrom" 
-                                id="fromCountry" 
-                                placeholder="Choose Country FROM" 
-                                autoFocus 
-                                required
-                            />
-                            <datalist id="countryFrom">       
-                                <option value="Serbia"/>
-                                <option value="Greece"/>
-                                <option value="Russia"/>
-                                <option value="USA"/>
-                            </datalist>
-                            <br/> <br/> 
-                            City :
-                            <input 
-                                list="cityFrom" 
-                                name="cityFrom" 
-                                placeholder="City from"/>
-                                    <br/>
-                            <datalist id="cityFrom">
-                                <option value="Belgrade"/>
-                                <option value="Thessaloniki"/>
-                                <option value="Chicago"/>
-                                <option value="Moscow"/>
-                            </datalist>
-                        </div>
-                        <div id="to">
-                            TO:
-                            <br/> 
-                            Country:
-                            <input 
-                                list="countryTo"
-                                name="countryTo" 
-                                id="toCountry" 
-                                placeholder="Choose Country TO" 
-                                required
-                            />
-                            <datalist id="countryTo">       
-                                        <option value="Serbia"/>
-                                        <option value="Greece"/>
-                                        <option value="Russia"/>
-                                        <option value="USA"/>
-                            </datalist>
-                            <br/> <br/> 
-                            City :
-                            <input list="cityTo" name="cityTo" placeholder="City to"/>
-                            <br/>
-                            <datalist id="cityTo">
-                                        <option value="Belgrade"/>
-                                        <option value="Thessaloniki"/>
-                                        <option value="Chicago"/>
-                                        <option value="Moscow"/>
-                            </datalist>
-                        </div>
-                        <div id="type">
-                            Transportation Type
-                            <br/> 
-                            <input type="radio" name="transportationType" id="airplane"/>
-                            <span>Airplane    </span>
-                            <br/>                        
-                            <input type="radio" name="transportationType" id="bus"/>
-                            <span>BUS</span>
-                            <br/> 
-                            <input type="radio" name="transportationType" id="companyCar"/>
-                            <span>Company car    </span>
-                            <br/>                        
-                            <input type="radio" name="transportationType" id="rentACar"/>
-                            <span>Rent a car</span>
-                            <br/>
-                            <input type="radio" name="transportationType" id="train"/>
-                            <span>Train   </span>
-                            <br/>                        
-                            <input type="radio" name="transportationType" id="taxi"/>
-                            <span>Taxi</span>
-                        </div>
+                        <TransportationFrom/>
+                        <TransportationTo/>
+                        <TransportationType/>
                         <div>
                             <br/><br/>
                             <button type="submit" onClick={showTransportation}>Submit</button>
