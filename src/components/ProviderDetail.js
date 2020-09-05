15177046639513da7dc9cd5e67e72be5f5ae145f
tatjama <tanja120a@gmail.com>
2020-09-05 07:16:25 +0200

    creaated component ProviderDetail

---
+++ src/components/ProviderDetail.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const ProviderDetail = () =>{
+
+    function closeProviderDetail(){
+        alert("Close provider detail")
+    }
+    return(
+    <div className="provider-detail " id="providerDetail">
+        <div className="left ">
+            <p>Name: AirSerbia</p>
+            <p>Country: Serbia</p>
+            <p>City: Belgrade</p>
+            <p>Adress: Bulevar umetnosti bb</p>
+            <p>Phone:+381-11-444-444</p>
+            <p>Contact:</p>
+        </div>
+        <div className="right ">
+            <h3>About company</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat
+               viverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet
+               faucibus maximus. Proin non nulla lectus.
+            </p>
+            <button onClick={closeProviderDetail}>
+                Close Details
+            </button>
+        </div>
+    </div>
+    )
+}
+
+export default ProviderDetail;
--- src/pages/Providers.js
+++ src/pages/Providers.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import SelectForm from '../components/SelectForm';
-
 import ProviderHeading from '../components/ProviderHeading';
+import ProviderDetail from '../components/ProviderDetail';
 
 const Providers = ()=>{
     function closeProviderEdit() {
         alert('Close provider Edit')
     }
-    function closeProviderDetail(){
-        alert("Close provider detail")
-    }
+    
    
     return(
         <div className="page-providers">
@@ -26,28 +24,8 @@ const Providers = ()=>{
                             <ProviderHeading/>
                             <div className="show-provider-detail " id="showProviderDetail ">
                                 <hr style={{"border":"3px solid #f1f1f1 "}}/>
-                                <div className="provider-detail " id="providerDetail">
-                                    <div className="left ">
-                                        <p>Name: AirSerbia</p>
-                                        <p>Country: Serbia</p>
-                                        <p>City: Belgrade</p>
-                                        <p>Adress: Bulevar umetnosti bb</p>
-                                        <p>Phone:+381-11-444-444</p>
-                                        <p>Contact:</p>
-                                    </div>
-                                    <div className="right ">
-                                        <h3>About company</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus neque et nulla dictum, ac scelerisque quam ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam volutpat
-                                             viverra dolor, ac efficitur libero commodo ac. Nam venenatis sodales sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis sed libero et purus feugiat blandit. Cras egestas lectus sit amet
-                                            faucibus maximus. Proin non nulla lectus.
-                                        </p>
-                                        <button onClick={closeProviderDetail}>
-                                        Close Details
-                                        </button>
-                                    </div>
-
-                                 </div>
-                        </div>
+                                 <ProviderDetail/>
+                            </div>
                         <form className="show-provider-edit " id="showEditProvider" >
                             <hr style={{"border":"3px solid #f1f1f1 "}}/>
 
