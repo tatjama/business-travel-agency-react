b6cd04907538086ce7599d63bc08ebec7cb1df46
tatjama <tanja120a@gmail.com>
2020-09-06 17:54:06 +0200

    countries on SelectForm done
    Countries put Array on SelectForm.
    Cities are depend from couintries

--- src/components/SelectForm.js
+++ src/components/SelectForm.js
@@ -7,69 +7,68 @@ const SelectForm = () =>{
     const countries = [
         {
             value: 0,
-            name: "Greece"
+            name: "Greece",
+            id: "country0",
+            cities: [
+                {value: 0, id: "country0-city0", name: "Thessaloniki"}, 
+                {value: 1, id: "country0-city1", name: "Athene"},
+                {value: 2, id: "country0-city2", name: "Chaniotis"}, 
+                {value: 3, id: "country0-city3",  name: "Rhodes"}
+                
+            ]
         },        
         {
             value: 1,
-            name: "Russia"
+            name: "Russia",
+            id: "country1",
+            cities: [
+                {value: 0, id: "country1-city0", name: "Moscow"}, 
+                {value: 1, id: "country1-city1", name: "Saint Petersburg"},
+                {value: 2, id: "country1-city2", name: "Astrakhan"}, 
+                {value: 3, id: "country1-city3",  name: "Yakutsk"}
+            ]
         },
         {
             value: 2,
-            name: "Serbia"            
+            name: "Serbia",  
+            id: "country2" ,
+            cities: [
+                {value: 0, id: "country2-city0", name: "Belgrade"}, 
+                {value: 1, id: "country2-city1", name: "Nis"},
+                {value: 2, id: "country2-city2", name: "Kraljevo"}, 
+                {value: 3, id: "country2-city3",  name: "Novi Sad"}, 
+                {value: 4, id: "country2-city3",  name: "Valjevo"}, 
+                {value: 5, id: "country2-city4",  name: "Cacak"
+            }]         
         },
         {
             value: 3,
-            name: "USA"
+            name: "USA",
+            id: "country3",
+            cities:  [
+                {value: 0, id: "country3-city0", name: "Washington"}, 
+                {value: 1, id: "country3-city1", name: "Chicago"},
+                {value: 2, id: "country3-city2", name: "Los Angeles"}, 
+                {value: 3, id: "country3-city3",  name: "NewYork"}
+            ]
         }
-    ]
-    const cities = [
-         ["Thessaloniki", "Athene", "Chaniotis", "Rhodes"],
-         ["Moscow", "Saint Petersburg", "Astrakhan", "Yakutsk"],
-         ["Belgrade", "Nis", "Novi Sad", "Valjevo", "Cacak"],
-         ["Washington", "Chicago", "Los Angeles", "NewYork"]
-    ]
-    /*const cities = [
-        {"Greece": ["Thessaloniki", "Athene", "Chaniotis", "Rhodes"]},
-        {"Russia": ["Moscow", "Saint Petersburg", "Astrakhan", "Yakutsk"]},
-        {"Serbia": ["Belgrade", "Nis", "Novi Sad", "Valjevo", "Cacak"]},
-        {"USA": ["Washington", "Chicago", "Los Angeles", "NewYork"]}
-    ]*/
-   // const [country, setCountry] = useState({value: 2,name: "Serbia" });
-   // const [city, setCity] = useState(cities[2])
+    ] 
    
-   /* const handleOnChangeSelect = (e) =>{
-       
-        console.log(e.target.value);
-        useState((country)=>{
-            console.log(e.target.value)
-            country = countries.filter((country) =>{
-                country.value == e.target.value
-            }) 
-            })
-            console.log(country)
-        }*/
-        
-       /*s let name, city = '';
-        switch(e.target.value){
-            case "0":  name = "Greece" ; city = cities[0];  break;
-            case "1": name = "Russia"; city = cities[1]; break;
-            case "2": name = "Serbia"; city = cities[2]; break;
-            case "3": name = "USA"; city = cities[3]; break;
-            default : name = "def"; break;
-        }*/
-       // console.log(name, city)
-       // alert('You select ' +e.target.value + " option, name " )
-       const [country, setCountry] = useState({})
+       const [country, setCountry] = useState(countries[0]);
+       const [cities, setCities] = useState(country.cities)
        const handleOnChangeSelect = (e) =>{
         /*selectedValue = e.target.value
          console.log(selectedValue)
          console.log(props.countries[selectedValue])*/
-         setCountry(countries[e.target.value])
-        console.log(country)
+         setCountry(countries[e.target.value]);
     }
     useEffect(() => {
-       console.log(country)
-    },[country])
+       console.log(country);
+       setCities(country.cities);
+       console.log(cities)
+    },[country, cities])
+
+      
      
     function chooseDestination(e) {
         e.preventDefault();
