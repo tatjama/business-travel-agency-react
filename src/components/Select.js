2b6d66f4be7286a2da164fbd2c4376c6cd4d3f4f
tatjama <tanja120a@gmail.com>
2020-09-06 14:50:40 +0200

    useState, useEffect on Selection component
    UseState for countries and useEffect in Select.js

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,27 +2,17 @@ import React from 'react';
 
 const Select = (props) =>{
    // const countries = props.countries;
+   //let selectedValue = 2;
    console.log(props)
-   const handleOnChangeSelect = (e) =>{
-       console.log(e.target.value);
-       let name = '';
-       switch(e.target.value){
-           case "1":  name = "Greece" ;break;
-           case "2": name = "Russia"; break;
-           case "3": name = "Serbia"; break;
-           case "4": name = "USA"; break;
-           default : name = "def"; break;
-       }
-       console.log(name)
-       alert('You select ' +e.target.value + " option, name " + name )
-   }
+   
 
+   
     return(
-        <select className="nameCountry" onChange={handleOnChangeSelect} autoFocus >       
-                        <option value={props.country1.value} name = {props.country1.name}>{props.country1.name}</option>
-                        <option value={props.country2.value} name = {props.country2.name}>{props.country2.name}</option>
-                        <option value={props.country3.value} name = {props.country3.name}>{props.country3.name}</option>
-                        <option value={props.country4.value} name = {props.country4.name}>{props.country4.name}</option>
+        <select className="nameCountry" name = "countries" id="countries" onChange={props.handleChange} autoFocus >       
+                        <option value={props.countries[0].value}>{props.countries[0].name}</option>
+                        <option value={props.countries[1].value}>{props.countries[1].name}</option>
+                        <option value={props.countries[2].value}>{props.countries[2].name}</option>
+                        <option value={props.countries[3].value}>{props.countries[3].name}</option>
                         
         </select>
     )
