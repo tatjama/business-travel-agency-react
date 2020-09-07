cd84b7f680bd42c83bb255c8192d9b4115d03849
tatjama <tanja120a@gmail.com>
2020-09-07 07:57:49 +0200

    Select component on TransportationFrom

--- src/components/TransportationFrom.js
+++ src/components/TransportationFrom.js
@@ -1,40 +1,97 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Select from './Select';
 
 const TransportationFrom = () =>{
+    const [countryFrom, setCountryFrom] = useState(countries[0]);
+    const [citiesFrom, setCitiesFrom] = useState(countryFrom.cities);
+    const [cityFrom, setCityFrom] = useState(countryFrom.cities[0]);
+
+    const handleSelectCountry = (e) =>{        
+         setCountryFrom(countries[e.target.value]);
+    }
+    useEffect(() => {
+            setCitiesFrom(countryFrom.cities);
+        },[countryFrom, citiesFrom])
+
+    const handleSelectCity = (e) =>{
+        setCityFrom(citiesFrom[e.target.value]);
+    }
+    useEffect(() => {
+        console.log(cityFrom)
+    },[cityFrom])
     return(
         <div id="from">
         FROM:
         <br/>
         Country: 
-        <input 
-            list="countriesFrom" 
-            name="countryFrom" 
-            id="fromCountry" 
-            placeholder="Choose Country FROM" 
-            autoFocus 
-            required
+        <Select
+             handleChange = {handleSelectCountry}
+             optionArray = {countries}
+             autoFocus = {true}
+             name = "fromCountries"
         />
-        <datalist id="countriesFrom">       
-            <option value="Serbia"/>
-            <option value="Greece"/>
-            <option value="Russia"/>
-            <option value="USA"/>
-        </datalist>
         <br/> <br/> 
         City :
-        <input 
-            list="citiesFrom" 
-            name="cityFrom" 
-            placeholder="City from"/>
-                <br/>
-        <datalist id="citiesFrom">
-            <option value="Belgrade"/>
-            <option value="Thessaloniki"/>
-            <option value="Chicago"/>
-            <option value="Moscow"/>
-        </datalist>
+            <Select
+                 handleChange = {handleSelectCity}
+                 optionArray = {citiesFrom}
+                 autoFocus = {false}
+                 name = "fromCities"
+            />
+            
     </div>
     )
 }
 
 export default TransportationFrom;
+
+const countries = [
+    {
+        value: 0,
+        name: "Greece",
+        id: "country0",
+        cities: [
+            {value: 0, id: "country0-city0", name: "Thessaloniki"}, 
+            {value: 1, id: "country0-city1", name: "Athene"},
+            {value: 2, id: "country0-city2", name: "Chaniotis"}, 
+            {value: 3, id: "country0-city3",  name: "Rhodes"}
+            
+        ]
+    },        
+    {
+        value: 1,
+        name: "Russia",
+        id: "country1",
+        cities: [
+            {value: 0, id: "country1-city0", name: "Moscow"}, 
+            {value: 1, id: "country1-city1", name: "Saint Petersburg"},
+            {value: 2, id: "country1-city2", name: "Astrakhan"}, 
+            {value: 3, id: "country1-city3",  name: "Yakutsk"}
+        ]
+    },
+    {
+        value: 2,
+        name: "Serbia",  
+        id: "country2" ,
+        cities: [
+            {value: 0, id: "country2-city0", name: "Belgrade"}, 
+            {value: 1, id: "country2-city1", name: "Nis"},
+            {value: 2, id: "country2-city2", name: "Kraljevo"}, 
+            {value: 3, id: "country2-city3",  name: "Novi Sad"}, 
+            {value: 4, id: "country2-city3",  name: "Valjevo"}, 
+            {value: 5, id: "country2-city4",  name: "Cacak"}
+        ]         
+    },
+    {
+        value: 3,
+        name: "USA",
+        id: "country3",
+        cities:  [
+            {value: 0, id: "country3-city0", name: "Washington"}, 
+            {value: 1, id: "country3-city1", name: "Chicago"},
+            {value: 2, id: "country3-city2", name: "Los Angeles"}, 
+            {value: 3, id: "country3-city3",  name: "NewYork"}
+        ]
+    }
+] 
+
--- src/components/TransportationTo.js
+++ src/components/TransportationTo.js
@@ -1,36 +1,97 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Select from './Select';
 
 const TransportationTo = () =>{
+    const [countryTo, setCountryTo] = useState(countries[0]);
+       const [citiesTo, setCitiesTo] = useState(countryTo.cities);
+       const [cityTo, setCityTo] = useState(countryTo.cities[0]);
+
+    const handleSelectCountry = (e) =>{        
+         setCountryTo(countries[e.target.value]);
+    }
+    useEffect(() => {
+            setCitiesTo(countryTo.cities);
+        },[countryTo, citiesTo])
+
+    const handleSelectCity = (e) =>{
+        setCityTo(citiesTo[e.target.value]);
+    }
+    useEffect(() => {
+        console.log(cityTo)
+    },[cityTo])
+ 
     return(
         <div id="to">
-                            TO:
-                            <br/> 
-                            Country:
-                            <input 
-                                list="countriesTo"
-                                name="countryTo" 
-                                id="toCountry" 
-                                placeholder="Choose Country TO" 
-                                required
-                            />
-                            <datalist id="countriesTo">       
-                                        <option value="Serbia"/>
-                                        <option value="Greece"/>
-                                        <option value="Russia"/>
-                                        <option value="USA"/>
-                            </datalist>
-                            <br/> <br/> 
-                            City :
-                            <input list="citiesTo" name="cityTo" placeholder="City to"/>
-                            <br/>
-                            <datalist id="citiesTo">
-                                        <option value="Belgrade"/>
-                                        <option value="Thessaloniki"/>
-                                        <option value="Chicago"/>
-                                        <option value="Moscow"/>
-                            </datalist>
-                        </div>
+             TO:
+        <br/> 
+          Country:
+         <Select
+             handleChange = {handleSelectCountry}
+             optionArray = {countries}
+             autoFocus = {false}
+             name = "toCountries"
+        />                            
+                             <br/> <br/> 
+         City :
+         <Select
+             handleChange = {handleSelectCity}
+             optionArray = {citiesTo}
+             autoFocus = {false}
+             name = "toCities"
+        />
+     </div>
     )
 }
 
 export default TransportationTo;
+
+const countries = [
+    {
+        value: 0,
+        name: "Greece",
+        id: "country0",
+        cities: [
+            {value: 0, id: "country0-city0", name: "Thessaloniki"}, 
+            {value: 1, id: "country0-city1", name: "Athene"},
+            {value: 2, id: "country0-city2", name: "Chaniotis"}, 
+            {value: 3, id: "country0-city3",  name: "Rhodes"}
+            
+        ]
+    },        
+    {
+        value: 1,
+        name: "Russia",
+        id: "country1",
+        cities: [
+            {value: 0, id: "country1-city0", name: "Moscow"}, 
+            {value: 1, id: "country1-city1", name: "Saint Petersburg"},
+            {value: 2, id: "country1-city2", name: "Astrakhan"}, 
+            {value: 3, id: "country1-city3",  name: "Yakutsk"}
+        ]
+    },
+    {
+        value: 2,
+        name: "Serbia",  
+        id: "country2" ,
+        cities: [
+            {value: 0, id: "country2-city0", name: "Belgrade"}, 
+            {value: 1, id: "country2-city1", name: "Nis"},
+            {value: 2, id: "country2-city2", name: "Kraljevo"}, 
+            {value: 3, id: "country2-city3",  name: "Novi Sad"}, 
+            {value: 4, id: "country2-city3",  name: "Valjevo"}, 
+            {value: 5, id: "country2-city4",  name: "Cacak"}
+        ]         
+    },
+    {
+        value: 3,
+        name: "USA",
+        id: "country3",
+        cities:  [
+            {value: 0, id: "country3-city0", name: "Washington"}, 
+            {value: 1, id: "country3-city1", name: "Chicago"},
+            {value: 2, id: "country3-city2", name: "Los Angeles"}, 
+            {value: 3, id: "country3-city3",  name: "NewYork"}
+        ]
+    }
+] 
+
