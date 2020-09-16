6b37586e13f4edca437c41e0d59b4b5910a3d997
tatjama <tanja120a@gmail.com>
2020-09-16 05:25:58 +0200

    Update login.js
    localStorage get users

--- src/components/utils/login.js
+++ src/components/utils/login.js
@@ -3,6 +3,8 @@ import {users} from '../data/users.json';
 export default function login (values) {
     let user = {};
     console.log(values)
+    let usersArray = users.concat(JSON.parse(localStorage.getItem('usersInLocalStorage')))
+    console.log(usersArray)
     for(let i = 0; i < users.length; i++){       
         if(users[i].email === values.email && users[i].password === values.password){
             user = users[i]; 
