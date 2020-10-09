1128571650339ab88ea7a96ee79a3650acd9528e
tatjama <tanja120a@gmail.com>
2020-10-09 19:18:23 +0200

    Update createCommentsArray.js

--- src/components/utils/createCommentsArray.js
+++ src/components/utils/createCommentsArray.js
@@ -1,7 +1,7 @@
 const createCommentsArray = (query, comments) =>{
     const commentsArrayFromStorage = JSON.parse(localStorage.getItem('commentsArray')) || []
     function checkIdOnArray(commentFromStorage){
-        return commentFromStorage.result_object.location_id == query
+        return commentFromStorage.result_object.location_id === query
     }
     const commentsFilterArray = commentsArrayFromStorage.filter(checkIdOnArray)
     const newCommentsFilterArray = commentsFilterArray.map(comment => comment.comment )
